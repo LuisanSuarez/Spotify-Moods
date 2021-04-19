2864beaded3921f6d71681e711944654d50d8cf9
Luisan <luisangelsc@gmail.com>
2021-04-18 20:28:39 -0600

    force deploy

--- src/views/PlayMusic.js
+++ src/views/PlayMusic.js
@@ -37,6 +37,14 @@ export default function PlayMusic({
 
   const contextTags = useTags();
 
+  const [force, deploy] = useState("this will be a");
+
+  useEffect(() => {
+    console.log("forced deploy");
+    force = force + "deploy";
+    deploy("forcing a deploy");
+  }, []);
+
   useEffect(async () => {
     const allTags = await playlistsService().getTags();
     setAllTags(allTags);
